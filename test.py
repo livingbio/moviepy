6d844e8bd21cbe3cd60b5e8040c2233714fe0b06
tim <tim.yellow@gmail.com>
2016-08-27 12:51:17 +0800

    finish profiling suite

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 # coding: utf-8
-
+import cProfile
+import os
+import pstats
 from moviepy.editor import *
 
 
@@ -8,17 +10,19 @@ def example_video1():
     nice_yo = TextClip(txt="Nice Yo", bg_color='white', fontsize=100)
     nice_yo = nice_yo.set_position(lambda t: (1 + t*90, 'center'))
 
-    color = ColorClip(col=(33, 0, 0), size=(800,450))
+    color = ColorClip(col=(33, 0, 0), size=(1280, 720))
 
-    return CompositeVideoClip([color, nice_yo]).set_duration(6)
+    return CompositeVideoClip([color, nice_yo]).set_duration(5)
 
 
 def main():
     video = example_video1()
-    video.write_videofile(fps=10, filename="a.mp4")
-    import os
-    os.system("open a.mp4")
+    video.write_videofile(fps=30, filename="a.mp4")
 
 
 if __name__ == "__main__":
-    main()
+    cProfile.run('main()', './profiling')
+    p = pstats.Stats('./profiling')
+    p.sort_stats('time').print_stats(20)
+
+    os.system("open a.mp4")
